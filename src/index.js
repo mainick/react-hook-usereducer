fea72350987f08f46b82c2913fe7c11499cc1dc5
mainick <Ronnyx83>
2020-11-25 10:28:12 +0100

    readme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import AppHookReducerTodos from "./components/AppHookReducerTodos";
 import AppHookReducerNotications from "./components/AppHookReducerNotifications";
-import AppHookContext from "./components/AppHookContext";
 
 ReactDOM.render(
     <React.StrictMode>
